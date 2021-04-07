30f06c1c5e685e26309a7a2c4d05a796a917ff4a
Sebastiano <pasca.sebastiano5@libero.it>
2021-04-07 20:48:33 +0200

    Milestone 1, 2, 3

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        querySearch: "",
+        researched: [],
+        stellarNumber: 5,
+        imgFoundSrc: 'https://image.tmdb.org/t/p/',
+        imgFoundSize: 'w500',
+    },
+    methods: {
+        search() {
+            axios
+                .get("https://api.themoviedb.org/3/search/movie", {
+                    params: {
+                        api_key: "7e6063e162f3f80112ea0b2bb45258db",
+                        query: this.querySearch,
+                        language: "it - IT",
+                    }
+                })
+                .then((result) => {
+                    console.log(result.data.results);
+                    this.researched = result.data.results;
+                })
+
+            axios
+                .get("https://api.themoviedb.org/3/search/tv", {
+                    params: {
+                        api_key: "7e6063e162f3f80112ea0b2bb45258db",
+                        query: this.querySearch,
+                        language: "it - IT",
+                    }
+                })
+                .then((result) => {
+                    console.log(result.data.results);
+                    this.researched = this.researched.concat(result.data.results);
+                })
+        },
+        stellarVote(vote_average) {
+            return Math.ceil(vote_average * this.stellarNumber / 10)
+        }
+    },
+})
+
+
+/* this.movies = this.movies.concat(results); */
+
+Vue.config.devtools = true;
